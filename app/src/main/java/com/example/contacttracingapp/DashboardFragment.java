ec688fe72d139fba029c9101dbbd49037479be35
maxjanney <majanney@me.com>
2021-02-08 13:17:27 -0500

    implement DashboardFragment

---
+++ app/src/main/java/com/example/contacttracingapp/DashboardFragment.java
@@ -0,0 +1,40 @@
+package com.example.contacttracingapp;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DashboardFragment extends Fragment {
+
+    Dashboard dashboardActivity;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof Dashboard) {
+            dashboardActivity = (Dashboard) context;
+        } else {
+            throw new RuntimeException("Must implement the Dashboard interface.");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
+        root.findViewById(R.id.start_button).setOnClickListener(v -> dashboardActivity.start());
+        root.findViewById(R.id.stop_button).setOnClickListener(v -> dashboardActivity.stop());
+        return root;
+    }
+
+    interface Dashboard {
+        void start();
+        void stop();
+    }
+}
--- app/src/main/java/com/example/contacttracingapp/MainActivity.java
+++ app/src/main/java/com/example/contacttracingapp/MainActivity.java
@@ -1,14 +1,64 @@
 package com.example.contacttracingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DashboardFragment.Dashboard {
+
+    DashboardFragment dashboardFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment tempFrag;
+
+        if ((tempFrag = fm.findFragmentById(R.id.dashboard_fragment)) instanceof DashboardFragment) {
+            dashboardFragment = (DashboardFragment) tempFrag;
+        } else {
+            dashboardFragment = new DashboardFragment();
+            fm.beginTransaction()
+                    .add(R.id.dashboard_fragment, dashboardFragment)
+                    .commit();
+        }
+
+        if (!hasGPSPermission()) {
+            requestLocationPermission();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(this, getString(R.string.app_name), Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+
+    @Override
+    public void start() {
+        Toast.makeText(this, "Starting Service", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void stop() {
+        Toast.makeText(this, "Stopping Service", Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean hasGPSPermission() {
+        return checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestLocationPermission() {
+        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 123);
     }
 }
