32f56bfc7b937362a0470bc328ef710fbae83317
maxjanney <majanney@me.com>
2021-02-16 09:01:44 -0500

    use Deque instead of ArrayList

--- app/src/main/java/com/example/contacttracingapp/TracingIDList.java
+++ app/src/main/java/com/example/contacttracingapp/TracingIDList.java
@@ -9,13 +9,14 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.time.LocalDate;
-import java.util.ArrayList;
+import java.util.ArrayDeque;
 
 public class TracingIDList {
     private static final String LIST_FILE = "list";
+    private static final int MAX_IDS = 14;
    
     private static TracingIDList instance;
-    private ArrayList<TracingID> ids;
+    private ArrayDeque<TracingID> ids;
 
     public static TracingIDList getInstance(Context context) {
         if (instance == null) {
@@ -25,8 +26,19 @@ public class TracingIDList {
     }
 
     public void generateID(LocalDate date, Context context) {
-        ids.add(new TracingID(date));
+        ids.addFirst(new TracingID(date));
+        if (ids.size() > MAX_IDS) {
+            ids.pollLast();
+        }
         saveIDs(context);
+    }
+
+    public TracingID getCurrentID() {
+        return ids.peekFirst();
+    }
+
+    public ArrayDeque<TracingID> getIds() {
+        return ids;
     }
 
     @SuppressWarnings("unchecked")
@@ -37,12 +49,12 @@ public class TracingIDList {
             // try and restore list from storage
             fis = context.openFileInput(LIST_FILE);
             ois = new ObjectInputStream(fis);
-            ids = (ArrayList<TracingID>) ois.readObject();
+            ids = (ArrayDeque<TracingID>) ois.readObject();
             ois.close();
             fis.close();
         } catch (Exception ignored) {
             // no previously stored list, so create it
-            ids = new ArrayList<>();
+            ids = new ArrayDeque<>();
         }
     }
 
