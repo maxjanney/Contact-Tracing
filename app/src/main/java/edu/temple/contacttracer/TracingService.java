4a6f1c5855c4bab52d28e4f8901666e66c091477
Max Janney <majanney@me.com>
2021-02-21 21:32:24 -0500

    include pending intent for TracingService

--- app/src/main/java/edu/temple/contacttracer/TracingService.java
+++ app/src/main/java/edu/temple/contacttracer/TracingService.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -83,11 +84,15 @@ public class TracingService extends Service {
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, UPDATE_DISTANCE, locationListener);
         }
 
+        Intent startMainActivity = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, startMainActivity, 0);
+
         createNotificationChannel();
         Notification notification = new Notification.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_baseline_location_on_24)
                 .setContentTitle(getString(R.string.notification_title))
                 .setContentText(getString(R.string.notification_text))
+                .setContentIntent(pendingIntent)
                 .build();
 
         startForeground(1, notification);
