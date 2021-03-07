319146118a9d251060ef6252d92543ce84d5866b
maxjanney <majanney@me.com>
2021-03-06 23:57:26 -0500

    TraceFrage does not work

--- app/src/main/java/edu/temple/contacttracer/MainActivity.java
+++ app/src/main/java/edu/temple/contacttracer/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.DatePicker;
@@ -19,20 +20,22 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import edu.temple.contacttracer.Tracing.SedentaryEvent;
 import edu.temple.contacttracer.Tracing.SedentaryEventContainer;
 import edu.temple.contacttracer.Tracing.TracingID;
 import edu.temple.contacttracer.Tracing.TracingIDContainer;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements DashboardFragment.Dashboard {
 
@@ -40,9 +43,11 @@ public class MainActivity extends AppCompatActivity implements DashboardFragment
     static final String TRACING_TOPIC = "TRACING";
     static final String TAG = "MainActivity";
 
+    FragmentManager fm;
     RequestQueue requestQueue;
     TracingIDContainer tracingIDList;
     Intent tracingIntent;
+    MyApplication myApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +58,7 @@ public class MainActivity extends AppCompatActivity implements DashboardFragment
             requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 123);
         }
 
-        FragmentManager fm = getSupportFragmentManager();
+        fm = getSupportFragmentManager();
         if (!(fm.findFragmentById(R.id.dashboard_fragment) instanceof DashboardFragment)) {
             fm.beginTransaction()
                     .add(R.id.dashboard_fragment, new DashboardFragment())
@@ -63,11 +68,41 @@ public class MainActivity extends AppCompatActivity implements DashboardFragment
         tracingIDList = TracingIDContainer.getInstance(this);
         tracingIntent = new Intent(this, TracingService.class);
         requestQueue = Volley.newRequestQueue(this);
+        myApp = (MyApplication) getApplicationContext();
 
         generateDailyID();
 
         subscribeToTopic(TRACKING_TOPIC);
         subscribeToTopic(TRACING_TOPIC);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent launchTraceFrag = getIntent();
+        if (launchTraceFrag != null) {
+            Bundle extra = launchTraceFrag.getExtras();
+            if (extra != null) {
+                long date = extra.getLong("date");
+                Location l = extra.getParcelable("location");
+                LatLng latLng = new LatLng(l.getLatitude(), l.getLongitude());
+                fm.beginTransaction()
+                        .replace(R.id.dashboard_fragment, TraceFragment.newInstance(latLng, new Date(date)))
+                        .commit();
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        myApp.setForeground(true);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        myApp.setForeground(false);
     }
 
     @Override
--- app/src/main/java/edu/temple/contacttracer/TracingMessengerService.java
+++ app/src/main/java/edu/temple/contacttracer/TracingMessengerService.java
@@ -1,5 +1,10 @@
 package edu.temple.contacttracer;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.location.Location;
 import android.util.Log;
 
@@ -11,25 +16,33 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import edu.temple.contacttracer.Tracing.SedentaryEvent;
 import edu.temple.contacttracer.Tracing.SedentaryEventContainer;
 import edu.temple.contacttracer.Tracing.TracingIDContainer;
 
 public class TracingMessengerService extends FirebaseMessagingService {
 
+    private static final String CHANNEL_ID = "tracing_messenger_service_idd";
+    private static final String CHANNEL_NAME = "tracing_messenger_name";
     private static final String TAG = "TracingMessengerService";
     private static final String TRACKING = "/topics/TRACKING";
     private static final String TRACING = "/topics/TRACING";
     private static final double TRACING_DISTANCE = 1.83;    // 1.83 meters ~ 6 feet
 
-    private SedentaryEventContainer container;
     private TracingIDContainer tracingIds;
+    private NotificationManager nm;
+    private MyApplication myApp;
 
     @Override
     public void onCreate() {
         super.onCreate();
         tracingIds = TracingIDContainer.getInstance(this);
-        container = SedentaryEventContainer.getSedentaryEventContainer(this, Constants.REPORTS_FILE);
+        myApp = (MyApplication) getApplicationContext();
+        nm = getSystemService(NotificationManager.class);
     }
 
     @Override
@@ -37,9 +50,10 @@ public class TracingMessengerService extends FirebaseMessagingService {
         super.onMessageReceived(remoteMessage);
         String from = remoteMessage.getFrom();
         if (from.equals(TRACKING)) {
-            handleReport(remoteMessage);
+            handleTrackingMsg(remoteMessage);
         } else if (from.equals(TRACING)) {
             // handle tracing message
+            handleTracingMsg(remoteMessage);
         }
     }
 
@@ -48,20 +62,67 @@ public class TracingMessengerService extends FirebaseMessagingService {
         super.onDestroy();
     }
 
-    private void handleReport(RemoteMessage remoteMessage) {
+    private void handleTrackingMsg(RemoteMessage remoteMessage) {
         JsonObject jsonObj = JsonParser.parseString(remoteMessage.getData().get("payload")).getAsJsonObject();
         SedentaryEvent recvSedentaryEvent = new Gson().fromJson(jsonObj, SedentaryEvent.class);
         recvSedentaryEvent.setDate();
-        SedentaryEvent mySedentaryEvent = container.getLatestSedentaryEvent();
+
+        SedentaryEvent mySedentaryEvent
+                = SedentaryEventContainer.getSedentaryEventContainer(this, Constants.SEDENTARY_EVENTS_FILE).getLatestSedentaryEvent();
+
         // ignore our own UUID
         if (mySedentaryEvent != null && isExternalID(recvSedentaryEvent.getUUID())) {
             Location recvLocation = recvSedentaryEvent.getLocation();
             Location myLocation = mySedentaryEvent.getLocation();
             // ignore sedentary event if it was not within range
             if (myLocation.distanceTo(recvLocation) <= TRACING_DISTANCE) {
+                SedentaryEventContainer container
+                        = SedentaryEventContainer.getSedentaryEventContainer(this, Constants.REPORTS_FILE);
                 container.addSedentaryEvent(recvSedentaryEvent);
                 Log.d(TAG, container.toString());
             }
+        }
+    }
+
+    private void handleTracingMsg(RemoteMessage remoteMessage) {
+        Log.d(TAG, "received");
+        try {
+            String json = remoteMessage.getData().get("payload");
+            JSONObject jsonObject = new JSONObject(json);
+            JSONArray uuids = jsonObject.getJSONArray("uuids");
+            SedentaryEventContainer recvSedentaryEvents
+                    = SedentaryEventContainer.getSedentaryEventContainer(this, Constants.REPORTS_FILE);
+
+            if (!isExternalMsg(uuids))
+                return;
+
+            for (int i = 0; i < uuids.length(); i++) {
+                String uuid = uuids.getString(i);
+                for (SedentaryEvent se : recvSedentaryEvents.getSedentaryEvents()) {
+                    if (uuid.equals(se.getUUID())) {
+                        Intent launchTraceFrag = new Intent(this, MainActivity.class);
+                        launchTraceFrag.putExtra("date", jsonObject.getLong("date"));
+                        launchTraceFrag.putExtra("location", se.getLocation());
+                        if (myApp.isInForeground()) {
+                            startActivity(launchTraceFrag);
+                        } else {
+                            // notify user of positive result
+                            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, launchTraceFrag, 0);
+                            createNotificationChannel();
+                            Notification notification = new Notification.Builder(this, CHANNEL_ID)
+                                    .setSmallIcon(R.drawable.ic_baseline_location_on_24)
+                                    .setContentTitle("Contact Tracing")
+                                    .setContentText("Someone you recently came in contact with tested positive!")
+                                    .setContentIntent(pendingIntent)
+                                    .build();
+
+                            nm.notify(1, notification);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
@@ -69,4 +130,21 @@ public class TracingMessengerService extends FirebaseMessagingService {
         return !(tracingIds.getCurrentID().getUUID().toString().equals(uuid));
     }
 
+    private boolean isExternalMsg(JSONArray uuids) throws JSONException {
+        SedentaryEventContainer mySedentaryEvents
+                = SedentaryEventContainer.getSedentaryEventContainer(this, Constants.SEDENTARY_EVENTS_FILE);
+        for (int i = 0; i < uuids.length(); i++) {
+            String otherUUID = uuids.getString(i);
+            for (SedentaryEvent se : mySedentaryEvents.getSedentaryEvents()) {
+                String myUUID = se.getUUID();
+                if (myUUID.equals(otherUUID))
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    private void createNotificationChannel() {
+        nm.createNotificationChannel(new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH));
+    }
 }
